20e63bb97592d7aa6f803c641f59f33a6a03d0b1
Bibhudatta Sahoo (DevOn) <b.sahoo@devon.nl>
2023-05-10 12:58:21 +0530

    add condtion for URI parameters

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::group(['prefix' => 'v1', 'as' => 'api.'], function () {
     /* These API endpoints will be available for guest users */
     Route::post('/login', [LoginController::class, 'login'])->name('login');
     Route::get('/teams', [TeamsController::class, 'getAllTeams'])->name('show.all.teams');
-    Route::get('/team/{teams}/players', [PlayersController::class, 'getTeamPlayers'])->name('show.team.players');
+    Route::get('/team/{teams}/players', [PlayersController::class, 'getTeamPlayers'])->where('teams', '[0-9]+')->name('show.team.players');
 
     Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/user', function (Request $request) {
@@ -33,16 +33,16 @@ Route::group(['prefix' => 'v1', 'as' => 'api.'], function () {
                 Route::post('/', [TeamsController::class, 'createTeam'])->name('create');
                 /* The "prefix" will make the API URI as /api/v1/team/{id}
                 The "as" will make the route name as  api.team.show */
-                Route::get('/{id}', [TeamsController::class, 'getTeam'])->name('show');
-                Route::put('/{id}', [TeamsController::class, 'updateTeam'])->name('edit');
-                Route::delete('/{id}', [TeamsController::class, 'deleteTeam'])->name('delete');
+                Route::get('/{id}', [TeamsController::class, 'getTeam'])->where('id', '[0-9]+')->name('show');
+                Route::put('/{id}', [TeamsController::class, 'updateTeam'])->where('id', '[0-9]+')->name('edit');
+                Route::delete('/{id}', [TeamsController::class, 'deleteTeam'])->where('id', '[0-9]+')->name('delete');
             });
 
             Route::group(['prefix' => 'player', 'as' => 'player.'], function () {
                 Route::post('/', [PlayersController::class, 'createPlayer'])->name('create');
-                Route::get('/{id}', [PlayersController::class, 'getPlayer'])->name('show');
-                Route::put('/{id}', [PlayersController::class, 'updatePlayer'])->name('edit');
-                Route::delete('/{id}', [PlayersController::class, 'deletePlayer'])->name('delete');
+                Route::get('/{id}', [PlayersController::class, 'getPlayer'])->where('id', '[0-9]+')->name('show');
+                Route::put('/{id}', [PlayersController::class, 'updatePlayer'])->where('id', '[0-9]+')->name('edit');
+                Route::delete('/{id}', [PlayersController::class, 'deletePlayer'])->where('id', '[0-9]+')->name('delete');
             });
         });
     });
